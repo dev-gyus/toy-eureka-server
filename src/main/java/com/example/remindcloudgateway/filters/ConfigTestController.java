01796e96a768fc1ee339064d5c8e478e0c3ced0c
GyuHyeong <cjsworbehd13@gmail.com>
2021-12-18 17:15:36 +0900

    docker 배포 및 datadog 연동테스트

--- src/main/java/com/example/remindcloudgateway/filters/ConfigTestController.java
+++ src/main/java/com/example/remindcloudgateway/filters/ConfigTestController.java
@@ -5,6 +5,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -18,5 +20,10 @@ public class ConfigTestController {
     @GetMapping("")
     public ResponseEntity<List<String>> getConfigs(){
         return new ResponseEntity<>(Arrays.asList(env.getProperty("test.token-expire"), env.getProperty("test.salt")), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> returnString(@RequestBody String inputString){
+        return new ResponseEntity<>(inputString, HttpStatus.OK);
     }
 }
