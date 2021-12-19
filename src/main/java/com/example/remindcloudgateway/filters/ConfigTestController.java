1545298d7dc3d274926197222679f78abb656203
GyuHyeong <cjsworbehd13@gmail.com>
2021-12-19 14:27:06 +0900

    logback 적용 및 datadog 연동

--- src/main/java/com/example/remindcloudgateway/filters/ConfigTestController.java
+++ src/main/java/com/example/remindcloudgateway/filters/ConfigTestController.java
@@ -1,6 +1,7 @@
 package com.example.remindcloudgateway.filters;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ConfigTestController {
     private final Environment env;
+
+    @GetMapping("hello")
+    public ResponseEntity<String> hello(){
+        String hello = "헬로 메소드 진입";
+        log.info(hello);
+        return new ResponseEntity<>(hello, HttpStatus.OK);
+    }
 
     @GetMapping("")
     public ResponseEntity<List<String>> getConfigs(){
