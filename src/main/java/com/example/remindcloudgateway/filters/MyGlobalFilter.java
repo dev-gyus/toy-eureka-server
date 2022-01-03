02f0718eee2aca8d86662731d483ac67835260bc
GyuHyeong <cjsworbehd13@gmail.com>
2022-01-04 00:54:53 +0900

    필터수정

--- src/main/java/com/example/remindcloudgateway/filters/MyGlobalFilter.java
+++ src/main/java/com/example/remindcloudgateway/filters/MyGlobalFilter.java
@@ -10,7 +10,6 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 public class MyGlobalFilter implements GlobalFilter {
-//
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("request in URI:{}", exchange.getRequest().getURI());
